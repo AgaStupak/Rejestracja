22064d03b10c870b3cb83d1f298248c3dd5baee8
agastupak <hane.aga@gmail.com>
2018-05-30 15:38:06 +0200

    removed unused using

--- RejestracjaBillenniumPraktyki/Controllers/HomeController.cs
+++ RejestracjaBillenniumPraktyki/Controllers/HomeController.cs
@@ -1,9 +1,5 @@
 ﻿using RejestracjaBillenniumPraktyki.DAL;
 using RejestracjaBillenniumPraktyki.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace RejestracjaBillenniumPraktyki.Controllers
--- RejestracjaBillenniumPraktyki/DAL/RegistrationContext.cs
+++ RejestracjaBillenniumPraktyki/DAL/RegistrationContext.cs
@@ -1,9 +1,6 @@
 ﻿using RejestracjaBillenniumPraktyki.Models;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Web;
+
 
 namespace RejestracjaBillenniumPraktyki.DAL
 {
